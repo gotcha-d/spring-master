c5cc05c8276f58384a0fd606083b5470f1e2892d
gotcha-d <gotcha.privatemail@gmail.com>
2026-03-14 18:53:11 +0900

    p169 第13章　更新系のREST API

--- 1802-shopping-rest-update/src/main/java/com/example/shopping/controller/ProductMaintenanceRestController.java
+++ 1802-shopping-rest-update/src/main/java/com/example/shopping/controller/ProductMaintenanceRestController.java
@@ -1,14 +1,18 @@
 package com.example.shopping.controller;
 
+import java.net.URI;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.shopping.exception.DataNotFoundException;
+import com.example.shopping.input.ProductMaintenanceInput;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.shopping.entity.Product;
 import com.example.shopping.service.ProductMaintenanceService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/products")
@@ -26,6 +30,45 @@ public class ProductMaintenanceRestController {
 
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable String id) {
-        return productMaintenanceService.findById(id);
+
+        Product product = productMaintenanceService.findById(id);
+        if (product == null) {
+            throw new DataNotFoundException("見つかりませんでした。ID:" + id);
+        }
+        return product;
     }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProduct(
+            @PathVariable String id,
+            @Validated @RequestBody ProductMaintenanceInput productMaintenanceInput
+    ) {
+        productMaintenanceInput.setId(id);
+        productMaintenanceService.update(productMaintenanceInput);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Void> create(
+        @Validated @RequestBody ProductMaintenanceInput productMaintenanceInput
+    ) {
+        Product product = productMaintenanceService.register(productMaintenanceInput);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(product.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id) {
+        productMaintenanceService.delete(id);
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void dataNotFoundHandler() {}
 }
